d0e14bcefeab84630c6ce56d0053a7f6ca84e686
Mattéo Benaissa <matteobenaissa@gmail.com>
2024-11-11 17:01:14 +0100

    menu paper feedback

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -12,6 +12,7 @@ namespace UI
         [SerializeField] private Button _playButton;
         [SerializeField] private Button _quitButton;
         [SerializeField] private Image _blackScreen;
+        [SerializeField] private Transform _paperFeedback;
 
         private void Start()
         {
@@ -23,6 +24,8 @@ namespace UI
 
         private void StartLevel()
         {
+            _paperFeedback.DOMove(_paperFeedback.position + _paperFeedback.forward * 3, 1f);
+            
             _quitButton.GetComponent<MenuButton>().SetIsClicked();
             _blackScreen.DOFade(1, 1f).OnComplete(_levelManager.NextLevel);
         }
